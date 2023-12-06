a71f52113f1d88354be5c0aa4235a57cc15a45ed
Yuri Gabriel Constantino <106029861+YuriGConstantino@users.noreply.github.com>
2023-12-06 17:42:14 -0300

    Update index.tsx

--- Simple Coffee Listing/src/components/CardCoffee/index.tsx
+++ Simple Coffee Listing/src/components/CardCoffee/index.tsx
@@ -25,7 +25,7 @@ export default function CardCoffee({ cards }: CardsProps){
                     <span className={styles.card__popular}>Popular</span> :
                     null
                 }
-                <img className={styles.card__img} src={cards.image} alt="" width={260} height={160} />
+                <img className={styles.card__img} src={cards.image} alt={cards.name}/>
 
             </div>
 
